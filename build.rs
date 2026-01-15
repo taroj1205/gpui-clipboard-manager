6dd597ef2c396f5741b5ac082002fce80837e842
Shintaro Jokagi <taroj1205@gmail.com>
2026-01-15 21:54:13 +0900

    fix(app): fix terminal opening up on opening

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
         res.set("FileDescription", "GPUI Clipboard Manager");
+        res.set("Subsystem", "windows");
         res.compile().unwrap();
     }
 }
