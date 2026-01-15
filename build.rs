83ed88fb6f1b9419b59bd7d3098828d2047f96ff
Shintaro Jokagi <taroj1205@gmail.com>
2026-01-15 21:20:45 +0900

    fix: remove deprecated winres API calls in build script

--- build.rs
+++ build.rs
@@ -2,7 +2,6 @@ fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
         res.set("FileDescription", "GPUI Clipboard Manager");
-        res.set_subsystem(winres::WindowsSubsystem::Windows);
         res.compile().unwrap();
     }
 }
