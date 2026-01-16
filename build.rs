d47e2674d9459a4526b2450493daebaeca63bf5a
Shintaro Jokagi <61367823+taroj1205@users.noreply.github.com>
2026-01-16 10:11:00 +0900

    chore: build.rs の削除

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set("FileDescription", "GPUI Clipboard Manager");
-        res.set("Subsystem", "windows");
-        res.compile().unwrap();
-    }
-}
